13953de47e2982d1172973a7810013979090e37d
Michael m <michaelbeltran80@gmail.com>
2024-04-29 23:04:06 -0500

    second file

---
+++ RutasArchivos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package historiasclinicas;
+
+/**
+ *
+ * @author mac
+ */
+public class RutasArchivos {
+ 
+    public static final String RutaDirectorio= "/Users/mac/NetBeansProjects/Programacion/ProyectoSalud/src/main/java/PDFhistoriasclinicas";
+}
+///Users/mac/NetBeansProjects/Programacion/ProyectoSalud/src/main/java
+        
+// "/Users/mac/documents/historiaclinica/"
